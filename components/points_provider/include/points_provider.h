219c21cdc78526b4e7e4566275e4ee23bc204e12
strangerintheq <tripmannn@gmail.com>
2026-08-29 17:38:29 +0300

    fsm calls to points provider

--- components/points_provider/include/points_provider.h
+++ components/points_provider/include/points_provider.h
@@ -1,9 +1,23 @@
 #ifndef POINTS_PROVIDER_H
 #define POINTS_PROVIDER_H
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
 void points_provider_start();
+
 void points_provider_pause();
+
 void points_provider_unpause();
+
 void points_provider_stop();
+
+void points_provider_init(
+    int capacity, 
+    int batch_size, 
+    QueueHandle_t queue, 
+    SemaphoreHandle_t sync
+);
 
 #endif
